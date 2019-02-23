0c537b9b02a4a398738cc6eebcf03eead0a3fd6c
Vander Borges <vanderpborges@hotmail.com>
2019-02-23 20:43:52 -0300

    Primeiro Commit

---
+++ src/main/java/com/vander/workshopmongo/WorkshopSpringMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.vander.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vander/workshopmongo/WorkshopSpringMongoDbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vander.workshopmongo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WorkshopSpringMongoDbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
